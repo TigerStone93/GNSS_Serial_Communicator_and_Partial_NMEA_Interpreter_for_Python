9ca7a06c63e0262e407900d5ff9959863909b543
TigerStone93 <46550846+TigerStone93@users.noreply.github.com>
2019-05-10 19:58:17 +0900

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+# ====================================================== #
+# Yonsei University - Seamless Transportation Laboratory #
+# Ho Suk                                                 #
+# sukho93@yonsei.ac.kr                                   #
+# ====================================================== #
+
+from NMEAInterpreter import*
+
+import time
+
+def main():
+    try:
+        MyNMEAInterpreter = NMEAInterpreter()
+            
+        while True:
+            MyNMEAInterpreter.convertDataUnit4PX2()
+            time.sleep(0.01)
+    finally:
+        MyNMEAInterpreter.MySerialCommunicator.ser.close()
+    
+if __name__ == "__main__":
+    main()
